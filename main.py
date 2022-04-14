af8b0fe52b12f25d1e8d08da8fceedc52e2c74eb
AbrahamH <75340597+Stonepaw90@users.noreply.github.com>
2022-04-14 11:49:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import streamlit as st
-#import pandas as pd
-#import random as rand
-#from PIL import Image
-import os
-st.write(os.listdir())
-st.stop()
+import pandas as pd
+import random as rand
+from PIL import Image
+
 # import os
 # os.system(r"cd /D C:\Users\Abraham\miniconda3\envs\snowflakes\Scripts | streamlit run main.py")
 # os.system("streamlit run main.py")
